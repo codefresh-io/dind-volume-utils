5d7a21c2403776928148feccc3f25b429442ceb3
kosta709 <kosta@codefresh.io>
2017-12-19 14:43:30 +0200

    added dind-metrics exporter

--- dind-metrics/metrics.sh
+++ dind-metrics/metrics.sh
@@ -3,73 +3,151 @@
 DIR=$(dirname $0)
 
 METRICS_DIR=${DIR}/../monitor/metrics
-METRIC_FILE=${METRICS_DIR}/lv_metrics.prom
-METRIC_FILE_TMP=${METRIC_FILE}.$$
 
 COLLECT_INTERVAL=15
 
 echo "Started $0 at $(date) on node $NODE_NAME
-METRIC_FILE=${METRIC_FILE}
 COLLECT_INTERVAL=${COLLECT_INTERVAL}
 "
 
-DOCKER_PS_OUT_FILE=/tmp/docker-ps.out
-
-DF_OUT_FILE=/tmp/df.out
-DF_INODES_OUT_FILE=/tmp/df-i.out
-if [[ $(uname) == "Linux" ]]; then
-   DF_OPTS="-B 1024"
+if [[ ! -d "${METRICS_DIR}" ]]; then
+   mkdir -p "${METRICS_DIR}"
 fi
 
+METRICS_dind_pvc_status="${METRICS_DIR}"/dind_pvc_status.prom
+METRICS_TMP_dind_pvc_status="${METRICS_DIR}"/dind_pvc_status.prom.tmp.$$
+
+echo "dind_pvc_status metric collected to ${METRICS_dind_pvc_status} "
+
+METRICS_dind_pod_status="${METRICS_DIR}"/dind_pod_status.prom
+METRICS_TMP_dind_pod_status="${METRICS_DIR}"/dind_pod_status.prom.tmp.$$
+echo "dind_pod_status metric collected to ${METRICS_dind_pod_status} "
+
+METRICS_dind_volume_pod_status="${METRICS_DIR}"/dind_volume_pod_status.prom
+METRICS_TMP_dind_volume_pod_status="${METRICS_DIR}"/dind_pod_volume_status.prom.tmp.$$
+echo "dind_volume_pod_status metric collected to ${METRICS_dind_volume_pod_status} "
+
+get_dind_pvc_metrics(){
+
+    local METRIC_NAME=${1:-"pvc_status"}
+    local LABEL_SELECTOR=${2:-'codefresh-app=dind'}
+
+    local TEMPLATE_GET_PVC='{{range .items}}{{.metadata.namespace}}{{"\t"}}{{.metadata.name}}{{"\t"}}{{.status.phase}}{{"\t"}}{{.spec.storageClassName}}'
+    TEMPLATE_GET_PVC+='{{"\t"}}{{index .metadata.labels "pod_namespace" }}{{"\t"}}{{index .metadata.labels "pod_name" }}'
+    TEMPLATE_GET_PVC+='{{"\t"}}{{index .metadata.labels "runtime_env" }}{{"\n"}}{{end}}'
+
+    local PVC_NAMESPACE
+    local PVC_NAME
+    local PHASE
+    local STORAGE_CLASS
+    local POD_NAMESPACE
+    local POD_NAME
+    local RUNTIME_ENV
+    local PVC_STATUS
+
+    local TMP_FILE=/tmp/get_dind_pvc_metrics.tmp.$$
+    ### Metric dind_pvc_status
+    kubectl get pvc -a --all-namespaces -l "$LABEL_SELECTOR" -ogo-template="$TEMPLATE_GET_PVC" > ${TMP_FILE}
+    cat ${TMP_FILE} | while read line
+    do
+       PVC_NAMESPACE=$(echo "$line" | cut -f1 )
+       PVC_NAME=$(echo "$line" | cut -f2 )
+       PHASE=$(echo "$line" | cut -f3)
+       STORAGE_CLASS=$(echo "$line" | cut -f4)
+       POD_NAMESPACE=$(echo "$line" | cut -f5)
+       POD_NAME=$(echo "$line" | cut -f6)
+       RUNTIME_ENV=$(echo "$line" | cut -f7)
+
+       case $PHASE in
+           Pending)
+              PVC_STATUS="0"
+           ;;
+           Bound)
+              PVC_STATUS="1"
+           ;;
+           Lost)
+              PVC_STATUS="-1"
+           ;;
+           *)
+              PVC_STATUS="-2"
+           ;;
+       esac
+       LABELS="pvc_namespace=\"${PVC_NAMESPACE}\",pvc_name=\"${PVC_NAME}\",storage_class=\"${STORAGE_CLASS}\",pod_name=\"${POD_NAME}\",pod_namespace=\"${POD_NAME}\",runtime_env=\"${POD_NAME}\""
+       if [[ -n "${PVC_STATUS}" ]]; then
+         echo "${METRIC_NAME}{$LABELS} ${PVC_STATUS}"
+       fi
+    done
+}
+
+get_dind_pod_status() {
+    ### dind_pod_status
+    local METRIC_NAME=${1:-"pod_status"}
+    local LABEL_SELECTOR=${2:-'app in (dind,runtime)'}
+
+    local TEMPLATE_GET_PODS='{{range .items}}{{.metadata.namespace}}{{"\t"}}{{.metadata.name}}{{"\t"}}{{.status.phase}}{{"\n"}}{{end}}'
+    local POD_NAMESPACE
+    local POD_NAME
+    local PHASE
+    local POD_STATUS
+
+    kubectl get pods -a --all-namespaces -l "${LABEL_SELECTOR}" -ogo-template="${TEMPLATE_GET_PODS}" | while read line
+    do
+       POD_NAMESPACE=$(echo "$line" | cut -f1)
+       POD_NAME=$(echo "$line" | cut -f2)
+       PHASE=$(echo "$line" | cut -f3)
+
+       case $PHASE in
+           Pending)
+              POD_STATUS="0"
+           ;;
+           Running)
+              POD_STATUS="1"
+           ;;
+           Succeeded)
+              POD_STATUS="2"
+           ;;
+           Failed)
+              POD_STATUS="3"
+           ;;
+           Unknown)
+              POD_STATUS="-1"
+           ;;
+           *)
+              POD_STATUS="-2"
+           ;;
+       esac
+       LABELS="pod_namespace=\"${POD_NAMESPACE}\",pod_name=\"${POD_NAME}\""
+       if [[ -n "${POD_STATUS}" ]]; then
+         echo "${METRIC_NAME}{$LABELS} ${POD_STATUS}"
+       fi
+    done
+}
+
+
 while true; do
-     rm -f "${METRIC_FILE_TMP}"
-
-     for ii in $(find "${VOLUME_PARENT_DIR}" -mindepth 1 -maxdepth 1 -type d -name "${VOLUME_DIR_PATTERN}")
-     do
-        VOLUME_PATH=$ii
-        LABELS="node_name=\"${NODE_NAME}\",volume_path=\"${VOLUME_PATH}\""
-
-        if [[ -f ${VOLUME_PATH}/created ]]; then
-           CREATED=$(awk 'END {print $1}' < ${VOLUME_PATH}/created)
-        else
-           CREATED="9999999999"
-        fi
-
-        if [[ -f ${VOLUME_PATH}/last_used ]]; then
-           LAST_USED=$(awk 'END {print $1}' < ${VOLUME_PATH}/last_used)
-        else
-           LAST_USED="9999999999"
-        fi
-
-        if [[ -f ${VOLUME_PATH}/pods ]]; then
-           MOUNTS_COUNT=$(wc -l < ${VOLUME_PATH}/pods)
-        else
-           MOUNTS_COUNT="0"
-        fi
-
-        cat <<EOF >> $METRIC_FILE_TMP
-# TYPE local_volume_creation_time gauge
-# HELP local_volume_creation_time - local volume creation timestamp
-local_volume_creation_time{$LABELS} ${CREATED}
-
-# TYPE local_volume_last_used_time gauge
-# HELP local_volume_last_used_time - local volume last_used timestamp
-local_volume_last_used_time{$LABELS} ${LAST_USED}
-
-# TYPE local_volume_mounts_count gauge
-# HELP local_volume_mounts_count - local volume mounts count
-local_volume_mounts_count{$LABELS} ${MOUNTS_COUNT}
+    cat <<EOF > "${METRICS_TMP_dind_pvc_status}"
+# TYPE dind_pvc_status gauge
+# HELP dind_pvc_status - status of dind pvc: 0 - Pending, 1 - Bound, -1 - Lost
 EOF
-        if [[ -f ${DIR_NAME}/deleted ]]; then
-        cat <<EOF >> $METRIC_FILE_TMP
-# TYPE local_volume_deleted_since gauge
-# HELP local_volume_deleted_since - local volume deletion timestamp
-local_volume_creation_time{$LABELS} ${CREATED}
+
+    cat <<EOF > "${METRICS_TMP_dind_pod_status}"
+# TYPE dind_pod_status gauge
+# HELP dind_pod_status - dind pod status 0 - Pending, 1 - Running, 2 - Succeded, 3 - Failed, -1 - Unknown
 EOF
-        fi
 
-     done
+    cat <<EOF > "${METRICS_TMP_dind_volume_pod_status}"
+# TYPE dind_volume_pod_status gauge
+# HELP dind_volume_pod_status - dind volume pod status 0 - Pending, 1 - Running, 2 - Succeded, -1 - Unknown
+EOF
 
-   mv ${METRIC_FILE_TMP} ${METRIC_FILE}
+    get_dind_pvc_metrics >> "${METRICS_TMP_dind_pvc_status}"
+    get_dind_pod_status "dind_pod_status" 'app in (dind,runtime)' >> "${METRICS_TMP_dind_pod_status}"
+    get_dind_pod_status "dind_volume_pod_status" 'codefresh-app=dind' >> "${METRICS_TMP_dind_volume_pod_status}"
+
+
+    mv "${METRICS_TMP_dind_pvc_status}" "${METRICS_dind_pvc_status}"
+    mv "${METRICS_TMP_dind_pod_status}" "${METRICS_dind_pod_status}"
+    mv "${METRICS_TMP_dind_volume_pod_status}" "${METRICS_dind_volume_pod_status}"
+
    sleep $COLLECT_INTERVAL
 done
