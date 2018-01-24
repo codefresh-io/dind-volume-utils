ce886957799c8cafdd06d264786366f687a9213c
kosta709 <kosta@codefresh.io>
2018-01-24 16:01:57 +0200

    added dind_pod_cpu_request metric

--- dind-metrics/metrics.sh
+++ dind-metrics/metrics.sh
@@ -16,12 +16,15 @@ fi
 
 METRICS_dind_pvc_status="${METRICS_DIR}"/dind_pvc_status.prom
 METRICS_TMP_dind_pvc_status="${METRICS_DIR}"/dind_pvc_status.prom.tmp.$$
-
 echo "dind_pvc_status metric collected to ${METRICS_dind_pvc_status} "
 
 METRICS_dind_pod_status="${METRICS_DIR}"/dind_pod_status.prom
 METRICS_TMP_dind_pod_status="${METRICS_DIR}"/dind_pod_status.prom.tmp.$$
 echo "dind_pod_status metric collected to ${METRICS_dind_pod_status} "
+
+METRICS_dind_pod_cpu_request="${METRICS_DIR}"/dind_pod_cpu_request.prom
+METRICS_TMP_dind_pod_cpu_request="${METRICS_DIR}"/dind_pod_cpu_request.tmp.$$
+echo "dind_pod_cpu_request metric collected to ${METRICS_dind_pod_cpu_request} "
 
 METRICS_dind_volume_pod_status="${METRICS_DIR}"/dind_volume_pod_status.prom
 METRICS_TMP_dind_volume_pod_status="${METRICS_DIR}"/dind_pod_volume_status.prom.tmp.$$
@@ -29,8 +32,7 @@ echo "dind_volume_pod_status metric collected to ${METRICS_dind_volume_pod_statu
 
 get_dind_pvc_metrics(){
 
-    local METRIC_NAME=${1:-"pvc_status"}
-    local LABEL_SELECTOR=${2:-'codefresh-app=dind'}
+    local LABEL_SELECTOR=${1:-'codefresh-app=dind'}
 
     local TEMPLATE_GET_PVC='{{range .items}}{{.metadata.namespace}}{{"\t"}}{{.metadata.name}}{{"\t"}}{{.status.phase}}{{"\t"}}{{.spec.storageClassName}}'
     TEMPLATE_GET_PVC+='{{"\t"}}{{index .metadata.labels "pod_namespace" }}{{"\t"}}{{index .metadata.labels "pod_name" }}'
@@ -74,27 +76,27 @@ get_dind_pvc_metrics(){
        esac
        LABELS="pvc_namespace=\"${PVC_NAMESPACE}\",pvc_name=\"${PVC_NAME}\",storage_class=\"${STORAGE_CLASS}\",dind_pod_name=\"${POD_NAME}\",dind_pod_namespace=\"${POD_NAME}\",runtime_env=\"${RUNTIME_ENV}\""
        if [[ -n "${PVC_STATUS}" ]]; then
-         echo "${METRIC_NAME}{$LABELS} ${PVC_STATUS}"
+         echo "dind_pvc_status{$LABELS} ${PVC_STATUS}" >> ${METRICS_TMP_dind_pvc_status}
        fi
     done
 }
 
 get_dind_pod_status() {
-    ### dind_pod_status
-    local METRIC_NAME=${1:-"pod_status"}
-    local LABEL_SELECTOR=${2:-'app in (dind,runtime)'}
+    local LABEL_SELECTOR=${1:-'app in (dind,runtime)'}
 
-    local TEMPLATE_GET_PODS='{{range .items}}{{.metadata.namespace}}{{"\t"}}{{.metadata.name}}{{"\t"}}{{.status.phase}}{{"\t"}}{{"\n"}}{{end}}'
+    local TEMPLATE_GET_PODS='{{range .items}}{{.metadata.namespace}}{{"\t"}}{{.metadata.name}}{{"\t"}}{{.status.phase}}{{"\t"}}{{(index .spec.containers 0).resources.requests.cpu}}{{"\t"}}{{"\n"}}{{end}}'
     local POD_NAMESPACE
     local POD_NAME
     local PHASE
     local POD_STATUS
+    local POD_CPU_REQUEST
 
     kubectl get pods -a --all-namespaces -l "${LABEL_SELECTOR}" -ogo-template="${TEMPLATE_GET_PODS}" | while read line
     do
        POD_NAMESPACE=$(echo "$line" | cut -f1)
        POD_NAME=$(echo "$line" | cut -f2)
        PHASE=$(echo "$line" | cut -f3)
+       POD_CPU_REQUEST=$(echo "$line" | cut -f4)
 
        case $PHASE in
            Pending)
@@ -118,7 +120,10 @@ get_dind_pod_status() {
        esac
        LABELS="dind_pod_namespace=\"${POD_NAMESPACE}\",dind_pod_name=\"${POD_NAME}\""
        if [[ -n "${POD_STATUS}" ]]; then
-         echo "${METRIC_NAME}{$LABELS} ${POD_STATUS}"
+         echo "dind_pod_status{$LABELS} ${POD_STATUS}" >> ${METRICS_TMP_dind_pod_status}
+       fi
+       if [[ -n "${POD_CPU_REQUEST}" && ( ${POD_STATUS} == 0 || ${POD_STATUS} == 1 ) ]]; then
+         echo "dind_pod_cpu_request{$LABELS} ${POD_CPU_REQUEST%m}" >> ${METRICS_TMP_dind_pod_cpu_request}
        fi
     done
 }
@@ -135,19 +140,19 @@ EOF
 # HELP dind_pod_status - dind pod status 0 - Pending, 1 - Running, 2 - Succeded, 3 - Failed, -1 - Unknown
 EOF
 
-    cat <<EOF > "${METRICS_TMP_dind_volume_pod_status}"
-# TYPE dind_volume_pod_status gauge
-# HELP dind_volume_pod_status - dind volume pod status 0 - Pending, 1 - Running, 2 - Succeded, -1 - Unknown
+    cat <<EOF > "${METRICS_TMP_dind_pod_cpu_request}"
+# TYPE dind_pod_cpu_request gauge
+# HELP dind_pod_cpu_request pod cpu requests in mCpu
 EOF
 
-    get_dind_pvc_metrics "dind_pvc_status" 'codefresh-app=dind' >> "${METRICS_TMP_dind_pvc_status}"
-    get_dind_pod_status "dind_pod_status" 'app in (dind,runtime)' >> "${METRICS_TMP_dind_pod_status}"
-    get_dind_pod_status "dind_volume_pod_status" 'codefresh-app=dind' >> "${METRICS_TMP_dind_volume_pod_status}"
+    get_dind_pvc_metrics 'codefresh-app=dind'
 
+    get_dind_pod_status 'app in (dind,runtime)'
+    get_dind_pod_status 'codefresh-app=dind'
 
     mv "${METRICS_TMP_dind_pvc_status}" "${METRICS_dind_pvc_status}"
     mv "${METRICS_TMP_dind_pod_status}" "${METRICS_dind_pod_status}"
-    mv "${METRICS_TMP_dind_volume_pod_status}" "${METRICS_dind_volume_pod_status}"
+    mv "${METRICS_TMP_dind_pod_cpu_request}" "${METRICS_dind_pod_cpu_request}"
 
    sleep $COLLECT_INTERVAL
 done
